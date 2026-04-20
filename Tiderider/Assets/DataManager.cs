cd8926295d2212c7c40b53fd37c96e6fab9d5cb8
Uzay Kuzey <114340199+uzaykuzey@users.noreply.github.com>
2026-04-20 20:57:26 +0300

    level complete bug fix (ışık did this technically)

--- Tiderider/Assets/DataManager.cs
+++ Tiderider/Assets/DataManager.cs
@@ -83,11 +83,6 @@ public static class DataManager
             gameData.highestUnlockedLevelIndex++;
             SaveGameData(gameData);
         }
-        else
-        {
-            gameData.highestUnlockedLevelIndex = completedLevelIndex;
-            SaveGameData(gameData);
-        }
     }
 
     /// <summary>
